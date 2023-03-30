59c081cef37abb4eb2fe25b3c6577122f1428138
Mogens Heller Grabe <mookid8000@gmail.com>
2023-03-30 23:11:30 +0200

    fix tests

--- Rebus.AzureBlobs.Tests/SasTokenTest.cs
+++ Rebus.AzureBlobs.Tests/SasTokenTest.cs
@@ -1,130 +1,90 @@
-﻿//using System;
-//using System.Collections.Concurrent;
-//using System.IO;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Azure.Storage.Blobs;
-//using NUnit.Framework;
-//using Rebus.Activation;
-//using Rebus.AzureBlobs.Tests.Extensions;
-//using Rebus.Config;
-//using Rebus.DataBus;
-//using Rebus.Routing.TypeBased;
-//using Rebus.Tests.Contracts;
-//using Rebus.Tests.Contracts.Extensions;
-//using Rebus.Transport.InMem;
-//// ReSharper disable RedundantArgumentDefaultValue
-//// ReSharper disable VariableHidesOuterVariable
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Sas;
+using NUnit.Framework;
+using Rebus.Activation;
+using Rebus.AzureBlobs.Tests.Extensions;
+using Rebus.Config;
+using Rebus.DataBus;
+using Rebus.Routing.TypeBased;
+using Rebus.Tests.Contracts;
+using Rebus.Tests.Contracts.Extensions;
+using Rebus.Transport.InMem;
+// ReSharper disable RedundantArgumentDefaultValue
+// ReSharper disable VariableHidesOuterVariable
 
-//namespace Rebus.AzureBlobs.Tests;
+namespace Rebus.AzureBlobs.Tests;
 
-//[TestFixture]
-//[Description("Verifies that endpoints can be started, even with the most restricted rights required to fulfill their purpose")]
-//public class SasTokenTest : FixtureBase
-//{
-//    BlobContainerClient _container;
+[TestFixture]
+[Description("Verifies that endpoints can be started, even with the most restricted rights required to fulfill their purpose")]
+public class SasTokenTest : FixtureBase
+{
+    BlobContainerClient _container;
 
-//    protected override void SetUp()
-//    {
-//        _container = new BlobContainerClient(AzureConfig.ConnectionString, Guid.NewGuid().ToString("N"));
+    protected override void SetUp()
+    {
+        _container = new BlobContainerClient(AzureConfig.ConnectionString, Guid.NewGuid().ToString("N"));
 
-//        Using(_container.AsDisposable(c => c.DeleteIfExists()));
-//    }
+        Using(_container.AsDisposable(c => c.DeleteIfExists()));
+    }
 
-//    [Test]
-//    public async Task ItWorks()
-//    {
-//        // now that the container is not automatically created, we need to do this
-//        await _container.CreateIfNotExistsAsync();
+    [Test]
+    public async Task ItWorks()
+    {
+        // now that the container is not automatically created, we need to do this
+        await _container.CreateIfNotExistsAsync();
 
-//        var sasWithReadAccess = GetSas(SharedAccessAccountPermissions.Read);
-//        var sasWithWriteAccess = GetSas(SharedAccessAccountPermissions.Write);
+        var sasWithReadAccess = GetSas(BlobContainerSasPermissions.Read);
+        var sasWithWriteAccess = GetSas(BlobContainerSasPermissions.Write);
 
-//        var receivedMessages = new ConcurrentQueue<string>();
-//        var network = new InMemNetwork();
+        var receivedMessages = new ConcurrentQueue<string>();
+        var network = new InMemNetwork();
 
-//        // this is the receiver
-//        using var receiver = new BuiltinHandlerActivator();
+        // this is the receiver
+        using var receiver = new BuiltinHandlerActivator();
 
-//        receiver.Handle<DataBusAttachment>(async attachment =>
-//        {
-//            using var source = await attachment.OpenRead();
-//            using var reader = new StreamReader(source);
+        receiver.Handle<DataBusAttachment>(async attachment =>
+        {
+            await using var source = await attachment.OpenRead();
+            
+            using var reader = new StreamReader(source);
 
-//            receivedMessages.Enqueue(await reader.ReadToEndAsync());
-//        });
+            receivedMessages.Enqueue(await reader.ReadToEndAsync());
+        });
 
-//        Configure.With(receiver)
-//            .Transport(t => t.UseInMemoryTransport(network, "receiver"))
-//            .DataBus(d => d.StoreInBlobStorage(new Uri(sasWithReadAccess)).DoNotUpdateLastReadTime())
-//            .Start();
+        Configure.With(receiver)
+            .Transport(t => t.UseInMemoryTransport(network, "receiver"))
+            .DataBus(d => d.StoreInBlobStorage(new Uri(sasWithReadAccess)).DoNotUpdateLastReadTime())
+            .Start();
 
-//        // create a sender
-//        using var sender = new BuiltinHandlerActivator();
+        // create a sender
+        using var sender = new BuiltinHandlerActivator();
 
-//        Configure.With(sender)
-//            .Transport(t => t.UseInMemoryTransport(network, "sender"))
-//            .DataBus(d => d.StoreInBlobStorage(new Uri(sasWithWriteAccess)).DoNotCreateContainer())
-//            .Routing(t => t.TypeBased().Map<DataBusAttachment>("receiver"))
-//            .Start();
+        Configure.With(sender)
+            .Transport(t => t.UseInMemoryTransport(network, "sender"))
+            .DataBus(d => d.StoreInBlobStorage(new Uri(sasWithWriteAccess)).DoNotCreateContainer())
+            .Routing(t => t.TypeBased().Map<DataBusAttachment>("receiver"))
+            .Start();
 
-//        // send attachment
-//        const string expectedText = "hey ven! 🍺";
+        // send attachment
+        const string expectedText = "hey ven! 🍺";
 
-//        using var bytes = new MemoryStream(Encoding.UTF8.GetBytes(expectedText));
-//        var attachment = await sender.Bus.Advanced.DataBus.CreateAttachment(bytes);
+        using var bytes = new MemoryStream(Encoding.UTF8.GetBytes(expectedText));
+        var attachment = await sender.Bus.Advanced.DataBus.CreateAttachment(bytes);
 
-//        await sender.Bus.Send(attachment);
+        await sender.Bus.Send(attachment);
 
-//        await receivedMessages.WaitUntil(q => q.Count == 1, timeoutSeconds: 5);
+        await receivedMessages.WaitUntil(q => q.Count == 1, timeoutSeconds: 5);
+    }
 
+    string GetSas(BlobContainerSasPermissions permissions)
+    {
+        var uri = _container.GenerateSasUri(permissions, DateTimeOffset.Now.AddDays(1));
 
-
-
-//        //            return;
-
-
-//        //            Console.WriteLine($@"Normal connection string:
-
-//        //    {AzureConfig.ConnectionString}
-
-//        //Container URI:
-
-//        //    {_container.Uri}
-
-//        //SAS token:
-
-//        //    {readSasToken}
-
-//        //Combined:
-
-//        //    {sasWithReadAccess}
-
-//        //");
-
-
-//        //            var blobs = readContainer.ListBlobs().ToList();
-
-//        //            Console.WriteLine($@"Found these blobs:
-
-//        //{string.Join(Environment.NewLine, blobs.Select(b => $"    {b.Uri}"))}
-
-//        //og det er fint");
-//    }
-
-//    string GetSas(SharedAccessAccountPermissions permissions)
-//    {
-//        var policy = new SharedAccessAccountPolicy
-//        {
-//            Permissions = permissions,
-//            Services = SharedAccessAccountServices.Blob,
-//            ResourceTypes = SharedAccessAccountResourceTypes.Container | SharedAccessAccountResourceTypes.Object,
-//            SharedAccessExpiryTime = DateTimeOffset.Now.AddDays(1),
-//            Protocols = SharedAccessProtocol.HttpsOrHttp,
-//        };
-
-//        var sasToken = _storageAccount.GetSharedAccessSignature(policy);
-
-//        return _container.Uri + sasToken;
-//    }
-//}
+        return uri.ToString();
+    }
+}
